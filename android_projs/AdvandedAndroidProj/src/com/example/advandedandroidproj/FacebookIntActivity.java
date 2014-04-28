b34ea1ccd30d89461c23cded74de38769035d00b
jimmy <xlogan777@yahoo.com>
2014-04-28 10:27:03 -0400

    updated comments to explain activity file for facebook integration.

--- android_projs/AdvandedAndroidProj/src/com/example/advandedandroidproj/FacebookIntActivity.java
+++ android_projs/AdvandedAndroidProj/src/com/example/advandedandroidproj/FacebookIntActivity.java
@@ -29,14 +29,23 @@ import android.widget.Toast;
  */
 public class FacebookIntActivity extends Activity 
 {
-	 //vars used for facebook.
+	 //vars used for facebook UI
 	 private TextView txtUserName;
      private Button btnLogin;
-     
+         
  	public static final String TAG = "FacebookIntActivity";
+ 	
+ 	//facebook object to manage the session
     private Facebook mFacebook;
+    
+    //app id that i got from FB, when i created the app to chat with..
+    //had to do this with facebook account.
     public static final String APP_ID = "620626734686580";
+    
+    //performs the request to facebook via url with my app id, via the facebook obj.
     private AsyncFacebookRunner mAsyncRunner;
+    
+    //this is wat i allow to view, the read stream and email.
     private static final String[] PERMS = new String[] { "read_stream" ,"email"};
 
 	@Override
@@ -56,8 +65,15 @@ public class FacebookIntActivity extends Activity
                 @Override
                 public void onClick(View view) 
                 {
-                	mFacebook = new Facebook(APP_ID);        
+                	//setup fb obj with app id
+                	mFacebook = new Facebook(APP_ID);
+                	
+                	//provide obj with with fb obj to do asynch processing.
                     mAsyncRunner = new AsyncFacebookRunner(mFacebook);
+                    
+                    //attempt to the authorization of my app id, with the
+                    //areas i want to access which is the read stream and email.
+                    //use this activity as the one to do the authorization.
 	                mFacebook.authorize(FacebookIntActivity.this, PERMS, new LoginDialogListener());
                 }
         	}
@@ -73,25 +89,35 @@ public class FacebookIntActivity extends Activity
 		{
 			Toast.makeText(getApplicationContext(),"logging out of FB!!!", Toast.LENGTH_SHORT).show();
 			
-			//logout
+			//logout from facebook session. using the application context.
 			mFacebook.logout(this.getApplicationContext());
 			
 		}
-		catch(Exception e){e.printStackTrace();}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
 	}
 
+	//private class that will check to see if the access was done correctly 
     private class LoginDialogListener implements DialogListener 
     {
             @Override
+            //this is the function to override that allows to be notified if the
+            //connection was done for login
             public void onComplete(Bundle values) 
             {
                     Log.d(TAG, "LoginONComplete");
+                    
+                    //get connection info
                     String token = mFacebook.getAccessToken();
                     long token_expires = mFacebook.getAccessExpires();
                     
                     Log.d(TAG, "AccessToken: " + token);
                     Log.d(TAG, "AccessExpires: " + token_expires);
                     
+                    //peform the request on "me" which is my app id, with an obj that
+                    //will read the data from the json obj .
                     mAsyncRunner.request("me", new IDRequestListener());
             }
 
@@ -114,9 +140,12 @@ public class FacebookIntActivity extends Activity
             }
     }//LoginDialogListener
 
+    //this class will do the listening for the data response back
+    //from the ansynch request done by the [mAsyncRunner.request("me", new IDRequestListener())]    
     private class IDRequestListener implements RequestListener
     { 
     	@Override
+    	//this will be called when we get a response back from te asynch.request function call
         public void onComplete(String response, Object state) 
     	{
     		try 
@@ -124,18 +153,25 @@ public class FacebookIntActivity extends Activity
                 Log.d(TAG, "IDRequestONComplete");
                 Log.d(TAG, "Response: " + response.toString());
                 
+                //get the json response from the function params.
+                //param needs to be "final" since it is being accessed by anonymous class.
                 final String myresponse = response;
                 
+                //invoke the UI features owned by the UI thread via this activity but run as a seperate
+                //thread
                 FacebookIntActivity.this.runOnUiThread
                 (new Runnable() 
                 	{
+                		//parse the json and get the info from the json obj.
                 		JSONObject json = Util.parseJson(myresponse);
                 		private TextView username = txtUserName;
+                		
                         private String id = json.getString("id");
                         private String name = json.getString("name");
                 		
                         public void run() 
                         {
+                        	//update the ui thread with the infor from the json string.
                         	username.setText("Welcome: " + name+"\n ID: "+id);
                         }
                 	}
