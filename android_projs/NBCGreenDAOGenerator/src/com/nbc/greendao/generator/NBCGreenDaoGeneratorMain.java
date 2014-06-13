188c9ea751529d4b53f9ee12ace49494a892e998
jimmy <xlogan777@yahoo.com>
2014-06-13 11:45:21 -0400

    added nbc green dao project generator to allow for green dao files to be generated.

---
+++ android_projs/NBCGreenDAOGenerator/src/com/nbc/greendao/generator/NBCGreenDaoGeneratorMain.java
@@ -0,0 +1,22 @@
+package com.nbc.greendao.generator;
+
+/**
+ * this will generate the java bean files for each entity class mapped to the
+ * DB table. It also generates the Entity DAO java file that does the java-sql mapping.
+ * Define the schema here with all the table relationships...1:1, 1:n..etc here.
+ * and the setup will have the associations in the java bean files and the DAO interface
+ * will contain these associations also.
+ * 
+ * Author J.Mena
+ *
+ */
+public class NBCGreenDaoGeneratorMain 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello world GreenDAO generator");
+
+	}
+	
+	//public static void 
+}
