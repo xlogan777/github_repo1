b2b594743dd7041995385446da4ccb0f110b7003
xlogan777 <xlogan777@yahoo.com>
2014-10-08 23:32:34 -0400

    updated code and added jar lib with config for log4j2 framework.

--- java_code/src/com/java_lang_exp/MainJavaLangExp.java
+++ java_code/src/com/java_lang_exp/MainJavaLangExp.java
@@ -1,14 +1,23 @@
 package com.java_lang_exp;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class MainJavaLangExp 
 {
+	static final Logger logger = 
+			LogManager.getLogger(MainJavaLangExp.class.getName());
+	  
 	public static void test0()
 	{
 		try
 		{
-			System.out.println("Test");		
-			JsonP_Example json_example1 = new JsonP_Example();
-			json_example1.parser();
+			logger.debug("Hello this is an debug message");
+			logger.info("Hello this is an info message");
+			logger.error("Hello this is an error message");
+		    
+			//JsonP_Example json_example1 = new JsonP_Example();
+			//json_example1.parser();
 		}
 		catch(Exception e)
 		{
