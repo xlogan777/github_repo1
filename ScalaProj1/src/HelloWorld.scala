646ce4d202d27866ee62a789ecc516ab5183c0b4
jimmy mena <xlogan777@yahoo.com>
2015-06-27 08:50:22 -0400

    added new scala proj.

---
+++ ScalaProj1/src/HelloWorld.scala
@@ -0,0 +1,12 @@
+
+object HelloWorld 
+{
+  /* This is my first java program.  
+    * This will print 'Hello World' as the output
+    */
+  def main(args: Array[String]) 
+  {
+    println("Hello, world!") // prints Hello World
+    println("Hello, world!") // prints Hello World
+  }
+}
