e2f947af0f0b5af7b230c7a6010f6cf7b29ea119
jimmy mena <xlogan777@yahoo.com>
2015-06-27 09:19:49 -0400

    update main scala class.

--- ScalaProj1/src/HelloWorld.scala
+++ ScalaProj1/src/HelloWorld.scala
@@ -6,7 +6,28 @@ object HelloWorld
     */
   def main(args: Array[String]) 
   {
-    println("Hello, world!") // prints Hello World
-    println("Hello, world!") // prints Hello World
+    println("Hello, world!"); // prints Hello World
+    println("Hello, world!"); // prints Hello World
+    
+    println("Hello\tWorld\n\n" );
+    
+    //mutable variable that can change value.
+    //use can use var or val to declare an variable.
+    //define the variable, then the type, then the value if u want to.
+    //not providing a variable type or set value is only for classes not objects.
+    var myVar : String = "Foo";//this is a mutable variable.
+    
+    val myVal : String = "myVal";//using val makes a constant
+    val test1 : Int = 100;//using val makes the variable a constant.
+    
+    //u can assign any variables at once like so.
+    //pair only works for 2 vars.
+    val (myVal1: Int, myVal2: String) = Pair(40, "Foo");//both of these are now immutable vars.
+    
+    var sum: Int = test1+myVal1;
+    println("sum = "+sum);
+    
+    //upto Scala Access Modifiers section in tutorialspoint.com
+    //http://www.tutorialspoint.com/scala/scala_access_modifiers.htm
   }
 }
