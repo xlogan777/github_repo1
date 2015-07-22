58bde72d2115a9c89bd9dc551f32fe3273494249
jimmy mena <xlogan777@yahoo.com>
2015-07-22 07:46:26 -0400

    updated comment

--- android_studio_proj/MyWeatherApp/app/src/main/java/jmtechsvcs/myweatherapp/MainWeatherActivity.java
+++ android_studio_proj/MyWeatherApp/app/src/main/java/jmtechsvcs/myweatherapp/MainWeatherActivity.java
@@ -27,6 +27,7 @@ public class MainWeatherActivity extends ActionBarActivity {
     private String MyTag = "MainWeatherActivity";
 //http://stackoverflow.com/questions/14744496/extract-database-of-an-application-from-android-device-through-adb
 
+    //test2
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
