741d430bdfa2e33abbfe2700d1366f15641bbde1
jimmy mena <xlogan777@yahoo.com>
2015-07-24 14:45:46 -0400

    updated code to allow for default usage of values when json data is missing. added fix for db processing of dao data.

--- android_studio_proj/MyWeatherApp/app/src/main/java/jmtechsvcs/myweatherapp/utils/WeatherJsonToDbProcessing.java
+++ android_studio_proj/MyWeatherApp/app/src/main/java/jmtechsvcs/myweatherapp/utils/WeatherJsonToDbProcessing.java
@@ -30,53 +30,32 @@ public class WeatherJsonToDbProcessing
             //main json obj for curr weather
             JSONObject curr_weather_obj = new JSONObject(jsonInput);
 
+            //this needs to be here since we need this..fail the processing.
             long city_id = curr_weather_obj.getLong("id");
             curr_weather_bean.setCity_id(city_id);
 
-            /*
-            "weather": [ //only use the first item in the json array here!
-        {
-            "id": 803,//weather condition id
-            "main": "Clouds",//group of weather params.(can be "Rain, Snow"..)
-            "description": "broken clouds",//weather desc in the group
-            "icon": "04n"//weather icon id, this shows the image icon
-        }
-    ],
-             */
+            //get the weather conditions..this json array needs to be here.
+            //with atleast 1 item., if this fails, dont continue...fail the processing.
             JSONArray weather_array = curr_weather_obj.getJSONArray("weather");
             JSONObject array_item = weather_array.getJSONObject(0);
-            long weather_id = array_item.getLong("id");
-            String weather_main = array_item.getString("main");
-            String weather_desc = array_item.getString("description");
-            String weather_icon = array_item.getString("icon");
 
+            long weather_id = WeatherMapUtils.getLongVal(array_item, "id");
+            String weather_main = WeatherMapUtils.getStringVal(array_item, "main");
+            String weather_desc = WeatherMapUtils.getStringVal(array_item, "description");
+            String weather_icon = WeatherMapUtils.getStringVal(array_item, "icon");
             curr_weather_bean.setCurr_weather_id(weather_id);
             curr_weather_bean.setCurr_weather_main(weather_main);
             curr_weather_bean.setCurr_weather_desc(weather_desc);
             curr_weather_bean.setCurr_weather_icon(weather_icon);
 
-
-            /*
-            "main": {
-        "temp": 296.03,//kelvin
-        "pressure": 1019,//atmospheric pressure, hPa units
-        "humidity": 83,//humidity %
-        "temp_min": 295.93,//kelvin
-        "temp_max": 296.15,//kelvin
-		"sea_level": 1020//atmospheric pressure, hPa units
-		"grnd_level":1010//atmospheric pressure, hPa units
-    },
-             */
-
-            JSONObject main_obj = curr_weather_obj.getJSONObject("main");
-            double main_temp = main_obj.getDouble("temp");
-            long main_pressure = main_obj.getLong("pressure");
-            long main_humidity = main_obj.getLong("humidity");
-            double main_temp_min = main_obj.getDouble("temp_min");
-            double main_temp_max = main_obj.getDouble("temp_max");
-            long main_sea_level = main_obj.getLong("sea_level");
-            long main_grnd_level = main_obj.getLong("grnd_level");
-
+            JSONObject main_obj = curr_weather_obj.optJSONObject("main");
+            double main_temp = WeatherMapUtils.getDoubleVal(main_obj,"temp");
+            long main_pressure = WeatherMapUtils.getLongVal(main_obj, "pressure");
+            long main_humidity = WeatherMapUtils.getLongVal(main_obj, "humidity");
+            double main_temp_min = WeatherMapUtils.getDoubleVal(main_obj,"temp_min");
+            double main_temp_max = WeatherMapUtils.getDoubleVal(main_obj,"temp_max");
+            long main_sea_level = WeatherMapUtils.getLongVal(main_obj, "sea_level");
+            long main_grnd_level = WeatherMapUtils.getLongVal(main_obj, "grnd_level");
             curr_weather_bean.setCurr_main_temp(main_temp);
             curr_weather_bean.setCurr_main_pressure(main_pressure);
             curr_weather_bean.setCurr_main_humidity(main_humidity);
@@ -85,65 +64,36 @@ public class WeatherJsonToDbProcessing
             curr_weather_bean.setCurr_main_sea_level(main_sea_level);
             curr_weather_bean.setCurr_main_grnd_level(main_grnd_level);
 
-            /*
-             "wind": {
-        "speed": 4.1,//meters/sec
-        "deg": 160//degrees
-    },
-             */
-
-            JSONObject wind_obj = curr_weather_obj.getJSONObject("wind");
-            double wind_speed = wind_obj.getDouble("speed");
-            long wind_degs = wind_obj.getLong("deg");
-
+            JSONObject wind_obj = curr_weather_obj.optJSONObject("wind");
+            double wind_speed = WeatherMapUtils.getDoubleVal(wind_obj, "speed");
+            long wind_degs = WeatherMapUtils.getLongVal(wind_obj, "deg");
             curr_weather_bean.setCurr_wind_speed(wind_speed);
             curr_weather_bean.setCurr_wind_degs(wind_degs);
 
-            /*
-            "clouds": {
-        "all": 75 //cloudiness %
-    },
+            JSONObject clouds_obj = curr_weather_obj.optJSONObject("clouds");
+            long clouds_all = WeatherMapUtils.getLongVal(clouds_obj, "all");
 
-    "rain":{"3h":3},//rain volume for the last 3 hrs
-	"snow":{"3h":3},//snow volume for the last 3 hrs
-             */
-            JSONObject clouds_obj = curr_weather_obj.getJSONObject("clouds");
-            long clouds_all = clouds_obj.getLong("all");
+            JSONObject rain_obj = curr_weather_obj.optJSONObject("rain");
+            long rain_3h = WeatherMapUtils.getLongVal(rain_obj, "3h");
 
-            JSONObject rain_obj = curr_weather_obj.getJSONObject("rain");
-            long rain_3h = rain_obj.getLong("3h");
-
-            JSONObject snow_obj = curr_weather_obj.getJSONObject("snow");
-            long snow_3h = snow_obj.getLong("3h");
+            JSONObject snow_obj = curr_weather_obj.optJSONObject("snow");
+            long snow_3h = WeatherMapUtils.getLongVal(snow_obj, "3h");
 
             curr_weather_bean.setCurr_clouds_all(clouds_all);
             curr_weather_bean.setCurr_rain_last3hrs(rain_3h);
             curr_weather_bean.setCurr_snow_last3hrs(snow_3h);
 
-            /*
-            "dt": 1437667656,//time of this calculation, unix utc
-             */
-
-            long curr_calc_time = curr_weather_obj.getLong("dt");
+            long curr_calc_time = WeatherMapUtils.getLongVal(curr_weather_obj, "dt");
             curr_weather_bean.setCurr_data_calc_time(curr_calc_time);
 
-            /*
-            "sys": {
-        "sunrise": 1437597912,//unix utc
-        "sunset": 1437638505//unix utc
-    }
-             */
-
-            JSONObject sys_obj = curr_weather_obj.getJSONObject("sys");
-            long sys_sunrise = sys_obj.getLong("sunrise");
-            long sys_sunset = sys_obj.getLong("sunset");
-
+            JSONObject sys_obj = curr_weather_obj.optJSONObject("sys");
+            long sys_sunrise = WeatherMapUtils.getLongVal(sys_obj, "sunrise");
+            long sys_sunset = WeatherMapUtils.getLongVal(sys_obj, "sunset");
             curr_weather_bean.setCurr_sys_sunrise_time(sys_sunrise);
             curr_weather_bean.setCurr_sys_sunset_time(sys_sunset);
 
             //save the data to the db.
             curr_weather_dao.insertOrReplace(curr_weather_bean);
-
         }
         catch (Exception e)
         {
--- android_studio_proj/MyWeatherApp/app/src/main/java/jmtechsvcs/myweatherapp/utils/WeatherMapUtils.java
+++ android_studio_proj/MyWeatherApp/app/src/main/java/jmtechsvcs/myweatherapp/utils/WeatherMapUtils.java
@@ -2,6 +2,8 @@ package jmtechsvcs.myweatherapp.utils;
 
 import android.util.Log;
 
+import org.json.JSONObject;
+
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
@@ -12,6 +14,12 @@ import java.io.Reader;
 public class WeatherMapUtils
 {
     private static final String LOGTAG = "WeatherMapUtils";
+
+    //these show that we have a failed retrieval of the value and replaced with this
+    //instead, showing we are missing data and do nothing.
+    private final static long DEFAULT_lONG_VAL = Long.MAX_VALUE;
+    private final static double DEFAULT_DOUBLE_VAL = Double.MAX_VALUE;
+    private final static String DEFAULT_STRING_VAL = "DEFAULT_STRING_VAL";
 
     //returns a json string from the input stream or empty string if something went wrong.
     public static String getJsonStringFromStream(InputStream inputStream)
@@ -41,5 +49,51 @@ public class WeatherMapUtils
         }
 
         return sb.toString();
+    }
+
+    /*
+        helper method to return a default value when json obj is null or when
+        the optional key just returns the default value if we didnt find it.
+     */
+    public static long getLongVal(JSONObject jsonObject, String key)
+    {
+        long rv;
+
+        if(jsonObject != null)
+            rv = jsonObject.optLong(key, DEFAULT_lONG_VAL);
+        else
+            rv = DEFAULT_lONG_VAL;
+
+        Log.d(LOGTAG, (rv == DEFAULT_lONG_VAL ? "using default long max val" : "long val = "+rv));
+
+        return rv;
+    }
+
+    public static double getDoubleVal(JSONObject jsonObject, String key)
+    {
+        double rv;
+
+        if(jsonObject != null)
+            rv = jsonObject.optDouble(key, DEFAULT_DOUBLE_VAL);
+        else
+            rv = DEFAULT_DOUBLE_VAL;
+
+        Log.d(LOGTAG, (rv == DEFAULT_DOUBLE_VAL ? "using default double max val" : "double val = "+rv));
+
+        return rv;
+    }
+
+    public static String getStringVal(JSONObject jsonObject, String key)
+    {
+        String rv;
+
+        if(jsonObject != null)
+            rv = jsonObject.optString(key, DEFAULT_STRING_VAL);
+        else
+            rv = DEFAULT_STRING_VAL;
+
+        Log.d(LOGTAG, (rv == DEFAULT_STRING_VAL ? "using default String val" : "string val = "+rv));
+
+        return rv;
     }
 }
