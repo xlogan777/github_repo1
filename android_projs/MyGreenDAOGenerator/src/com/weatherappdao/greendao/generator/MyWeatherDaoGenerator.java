624aeb0698bf320372f51779e556b46f04119b50
jimmy mena <xlogan777@yahoo.com>
2015-07-31 17:55:43 -0400

    fixed issue with icon table..removed not null keyword from auto-increment for primary key.

--- android_projs/MyGreenDAOGenerator/src/com/weatherappdao/greendao/generator/MyWeatherDaoGenerator.java
+++ android_projs/MyGreenDAOGenerator/src/com/weatherappdao/greendao/generator/MyWeatherDaoGenerator.java
@@ -161,7 +161,7 @@ public class MyWeatherDaoGenerator
       //add pk
       //this will be an incrementing row number to give unique row access
       //this is the auto-increment feature seen in other databases.
-      weather_icon_entity.addIdProperty().primaryKey().autoincrement().notNull();
+      weather_icon_entity.addIdProperty().autoincrement().primaryKey();
       
       //add non pk fields
       //searching will be done using this property.
