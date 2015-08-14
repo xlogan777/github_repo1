a8724c3c2e4687212288c78656907e0c19330df3
Jimbo <jimmy.mena@bbumail.com>
2015-08-14 17:01:09 -0400

    added comment

--- android_studio_proj/MyWeatherApp/app/src/main/java/jmtechsvcs/myweatherapp/networkpkg/WeatherMapUrls.java
+++ android_studio_proj/MyWeatherApp/app/src/main/java/jmtechsvcs/myweatherapp/networkpkg/WeatherMapUrls.java
@@ -13,6 +13,13 @@ public class WeatherMapUrls
     private final static String CURRENT_WEATHER_END_PT = "http://api.openweathermap.org/data/2.5/weather?";
     private final static String WEATEHR_ICON_END_PT = "http://openweathermap.org/img/w/";
 
+    //TODO:
+    /*
+        may need to get the updated time from the xml feed based current data
+        since there is an issue with the current time, sunrise, sunset times in utc..
+        vs the one from the xml feed.
+     */
+
     public static String getCurrentWeatherByCityId(String cityId)
     {
         String rv = CURRENT_WEATHER_END_PT+"id="+cityId+"&"+APP_ID;
