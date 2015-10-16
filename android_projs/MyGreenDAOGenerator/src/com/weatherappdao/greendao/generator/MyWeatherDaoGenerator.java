f08b66bfd900100f66a56d91fcb81feff9b68388
Jimbo <jimmy.mena@bbumail.com>
2015-10-16 10:24:16 -0400

    removed comment.

--- android_projs/MyGreenDAOGenerator/src/com/weatherappdao/greendao/generator/MyWeatherDaoGenerator.java
+++ android_projs/MyGreenDAOGenerator/src/com/weatherappdao/greendao/generator/MyWeatherDaoGenerator.java
@@ -6,7 +6,6 @@ import de.greenrobot.daogenerator.Schema;
 
 public class MyWeatherDaoGenerator 
 {
-   //test 3
 	public static void main(String[] args)
 	{
 		//create green dao schema obj with version number, and pkg for the generated files.
