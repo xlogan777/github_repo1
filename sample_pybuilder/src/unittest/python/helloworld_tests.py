9376fa44c2d54c12aa93edbc980880fae318ce44
Jimmy Mena <jmena@bbumail.com>
2018-05-23 09:16:01 -0400

    udpated code

--- sample_pybuilder/src/unittest/python/helloworld_tests.py
+++ sample_pybuilder/src/unittest/python/helloworld_tests.py
@@ -6,5 +6,5 @@ from helloworld import helloworld
 class HelloWorldTest(unittest.TestCase):
    def test_should_issue_hello_world_message(self):
       rv = helloworld("JIMMY")
-      self.assertEqual('collado'.upper(), rv)
-      #self.assertEqual("mena".upper(), rv)
+      #self.assertEqual('collado'.upper(), rv)
+      self.assertEqual("mena".upper(), rv)
